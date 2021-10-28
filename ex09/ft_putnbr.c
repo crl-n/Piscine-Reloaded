9c3f128e298ae4a717f3e145ef0af92b5d7ef4d5
Carl Nysten <cnysten@c1r6p14.hive.fi>
2021-10-28 14:05:51 +0300

    ft_ft.c, ft_putchar.c, ft_putnbr.c, ft_putstr.c  added

--- ex09/ft_putnbr.c
+++ ex09/ft_putnbr.c
@@ -1,16 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/28 10:55:46 by cnysten           #+#    #+#             */
-/*   Updated: 2021/10/28 11:10:58 by cnysten          ###   ########.fr       */
+/*   Created: 2021/10/28 11:04:15 by cnysten           #+#    #+#             */
+/*   Updated: 2021/10/28 13:56:29 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+#include <unistd.h>
+#include <stdlib.h>
+void	ft_putstr(char *s);
+
+int		ft_last_id(int nbr)
 {
-	*nbr = 42;
+	int	len;
+
+	len = 0;
+	while (nbr > 0)
+	{
+		len++;
+		nbr = nbr / 10;
+	}
+	return (len - 1);
+}
+
+void	ft_putnbr(int nbr)
+{
+	char	*s;
+	int		i;
+	
+	i = ft_last_id(nbr);
+	s = malloc((i + 1) * sizeof (char));
+	if (!s)
+		exit(-1);
+	while (i >= 0)
+	{
+		s[i--] = '0' + nbr % 10;
+		nbr = nbr / 10;
+	}
+	ft_putstr(s);
 }
