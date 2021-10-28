ea2de13b4ad4fcb081c3d1999c73c8f977ff8c6f
Carl Nysten <cnysten@c1r6p14.hive.fi>
2021-10-28 10:36:33 +0300

    ft_print_alphabet.c fixed compile error

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,13 +6,13 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 08:37:07 by cnysten           #+#    #+#             */
-/*   Updated: 2021/10/28 10:01:19 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/10/28 10:34:15 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_print_alphabet(char **s)
+void	ft_print_alphabet(void)
 {
 	char	c;
 
