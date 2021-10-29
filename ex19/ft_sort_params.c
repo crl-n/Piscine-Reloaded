0484d2fe3262e12f2f881d51e5681c4db3209c41
Carl Nysten <cnysten@c1r6p14.hive.fi>
2021-10-29 10:51:29 +0300

    ex19 with main

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 21:01:01 by cnysten           #+#    #+#             */
+/*   Updated: 2021/10/29 10:46:42 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_print(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+		ft_putstr(argv[i++]);
+}
+
+void	ft_print_sorted(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_print(argc, argv);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putstr(argv[1]);
+	else if (argc > 2)
+		ft_print_sorted(argc, argv);
+	return (0);
+}
