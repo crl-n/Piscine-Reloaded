134f3abcab3a7c21dc6ada5affaa28eaae657336
Carl Nysten <cnysten@c1r6p14.hive.fi>
2021-10-29 16:50:54 +0300

    ft_print_params fixed

--- ex18/ft_putchar.c
+++ ex18/ft_putchar.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/28 20:38:36 by cnysten           #+#    #+#             */
-/*   Updated: 2021/10/29 16:50:13 by cnysten          ###   ########.fr       */
+/*   Created: 2021/10/28 10:01:55 by cnysten           #+#    #+#             */
+/*   Updated: 2021/10/28 10:02:10 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
 
-int	main(int argc, char **argv)
+void	ft_putchar(char c)
 {
-	int		i;
-	char	*str;
-
-	i = 1;
-	while (i <= argc - 1)
-	{
-		str = argv[i++];
-		while (*str)
-			ft_putchar(*str++);
-		ft_putchar('\n');
-	}
-	return (0);
+	write(1, &c, 1);
 }
