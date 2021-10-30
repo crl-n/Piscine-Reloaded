a3568a17bbd0aa9df218f572d31614ddf3472d99
Carl Nysten <cnysten@c1r6p15.hive.fi>
2021-10-30 14:19:43 +0300

    ex13 fixed overflow issue

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 15:55:00 by cnysten           #+#    #+#             */
-/*   Updated: 2021/10/28 16:30:54 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/10/30 14:19:13 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
