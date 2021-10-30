959296620990fdb4a80f8ea91249c737b92dd0a3
Carl Nysten <cnysten@c1r6p15.hive.fi>
2021-10-30 14:18:41 +0300

    ex12 fixed maxint issue

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 15:00:01 by cnysten           #+#    #+#             */
-/*   Updated: 2021/10/28 15:06:20 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/10/30 14:17:28 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_iterative_factorial(int nb)
 	int	res;
 
 	res = 1;
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
